0c5645ccef59c0ef0a9468ac4ac2af7265e461bb
Wil Chung <iamwil@gmail.com>
2013-04-25 10:48:23 -0700

    remove resque from cheetah
    This is because we might want to use other 
    queuing services.

--- lib/cheetah/messenger/resque_messenger.rb
+++ lib/cheetah/messenger/resque_messenger.rb
@@ -1,16 +1,16 @@
-require 'resque'
+#require 'resque'
 
 module Cheetah
   # this is both extends Messenger and implements the Resque worker interface
-  class ResqueMessenger < Messenger
-    @queue = :cheetah
+  #class ResqueMessenger < Messenger
+  #  @queue = :cheetah
 
-    def do_send(message)
-      Resque.enqueue(self.class, message, @options)
-    end
+  #  def do_send(message)
+  #    Resque.enqueue(self.class, message, @options)
+  #  end
 
-    def self.perform(message, options)
-      Messenger.new(options).do_request(message)
-    end
-  end
+  #  def self.perform(message, options)
+  #    Messenger.new(options).do_request(message)
+  #  end
+  #end
 end
