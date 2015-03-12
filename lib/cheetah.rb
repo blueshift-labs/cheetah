efabd3b977b58e59f6ddc50fbdd3b370267aa5e7
Kiran Chaudhari <kiran@joshsoftware.com>
2015-03-12 13:36:59 +0530

    update mailgo api call

--- lib/cheetah.rb
+++ lib/cheetah.rb
@@ -48,11 +48,8 @@ module Cheetah
     #Create new mailing
     #Send mail set parameter {"approve" => 1, "send" => 1}
     #Test send set parameter {"test" => 8, "tester" => "test@example.com"}
-    def mailgo(name, subject, from, params = {})
+    def mailgo(params = {})
       path = "/cgi-bin/api/mailgo1"
-      params['name'] = name
-      params['subject'] = subject
-      params['from'] = from
       response = @messenger.do_request(Message.new(path, params))
       response.body.match(/\d{1,12}/)[0]
     end
